82a66986c110ab59d233c17f779830a9a6ba9c37
Bryan Cusme <bryan.cusme@devoteam.com>
2023-02-20 17:59:46 +0100

    add: backend projects

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,41 +1,41 @@
 /* eslint-disable @next/next/no-img-element */
-import { iconResolver } from '@/utils/iconResolver';
-import Link from 'next/link';
-import React from 'react';
+import { iconResolver } from "@/utils/iconResolver";
+import Link from "next/link";
+import React from "react";
 
 function ProjectCard({ project }) {
   const { slug, title, shortDescription, technologies, images, category } =
     project;
   return (
-    <div className='w-full'>
-      <div className='c-card block overflow-hidden rounded-lg bg-white shadow-md hover:shadow-xl'>
-        <div className='relative overflow-hidden pb-60'>
-          <a href={`/projects/${slug}`}>
+    <div className="w-full">
+      <div className="c-card block overflow-hidden rounded-lg bg-white shadow-md hover:shadow-xl">
+        <div className="relative overflow-hidden pb-60">
+          <Link href={`/projects/${slug}`}>
             <img
-              className='absolute inset-0 h-full w-full object-cover'
+              className="absolute inset-0 h-full w-full object-cover"
               src={images.cover.src}
               alt={`Go to ${title} poject page`}
             />
-          </a>
+          </Link>
         </div>
-        <div className='p-4 '>
-          <span className='inline-block rounded-full bg-secondary p-2 text-xs font-semibold uppercase leading-none tracking-wide text-white'>
+        <div className="p-4 ">
+          <span className="inline-block rounded-full bg-secondary p-2 text-xs font-semibold uppercase leading-none tracking-wide text-white">
             {category}
           </span>
-          <h2 className='text-xl mt-2 mb-2 font-bold'>{title}</h2>
-          <div className='my-3 flex gap-1 '>
+          <h2 className="text-xl mt-2 mb-2 font-bold">{title}</h2>
+          <div className="my-3 flex gap-2 ">
             {technologies.map((tech) => (
               <div key={tech.id + slug}>
-                {iconResolver(tech.id, 60, tech.name)}
+                {iconResolver(tech.id, 45, tech.name)}
               </div>
             ))}
           </div>
-          <p className='text-clamp'>{shortDescription.en}</p>
+          <p className="text-clamp">{shortDescription.en}</p>
         </div>
-        <div className='border-t border-b p-4 text-xs text-gray-700 '>
-          <Link href={`/projects/${slug}`} className='flex gap-1 items-center'>
+        <div className="border-t border-b p-4 text-xs text-gray-700 ">
+          <Link href={`/projects/${slug}`} className="flex gap-1 items-center">
             <p>Visit project </p>
-            {iconResolver('redirect', 15, 'redirect')}
+            {iconResolver("redirect", 15, "redirect")}
           </Link>
         </div>
       </div>
--- pages/projects/[projectId].jsx
+++ pages/projects/[projectId].jsx
@@ -1,67 +1,68 @@
 // TODO: Refactor to use the dynamic fetch via useRouter
-import React from 'react';
-import { useRouter } from 'next/router';
-import Image from 'next/image';
-import MainLayout from '@/components/MainLayout';
-import { iconResolver } from '@/utils/iconResolver';
-import Slider from '@/components/Slider';
-import Link from 'next/link';
-import projects from '@/public/assets/json/projects.json';
+//TODO: Feat: Add page based on backend - FE/FS tech
+import React from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import MainLayout from "@/components/MainLayout";
+import { iconResolver } from "@/utils/iconResolver";
+import Slider from "@/components/Slider";
+import Link from "next/link";
+import projects from "@/public/assets/json/projects.json";
 
 const project = {
-  slug: 'afstore',
-  title: 'AF Store',
-  category: 'fullstack',
+  slug: "afstore",
+  title: "AF Store",
+  category: "fullstack",
   shortDescription: {
-    en: 'A Fullstack ecommerce app developed with Remix and Strapi',
-    es: 'Aplicación fullstack desarrollada con Remix y Strapi',
+    en: "A Fullstack ecommerce app developed with Remix and Strapi",
+    es: "Aplicación fullstack desarrollada con Remix y Strapi",
   },
   longDescription: {
-    en: 'Sneakers ecommerce inspired in the Nike Air Force sneaker collection. The Frontend of this project has been made with Remix and the Backend with Strapi, a headless CMS.',
-    es: 'Sneakers ecommerce inspired in the Nike Air Force sneaker collection. The Frontend of this project has been made with Remix and the Backend with Strapi, a headless CMS.',
+    en: "Sneakers ecommerce inspired in the Nike Air Force sneaker collection. The Frontend of this project has been made with Remix and the Backend with Strapi, a headless CMS.",
+    es: "Sneakers ecommerce inspired in the Nike Air Force sneaker collection. The Frontend of this project has been made with Remix and the Backend with Strapi, a headless CMS.",
   },
   links: {
-    github: 'https://github.com/BrianC9/af-store-remix',
-    preview: 'https://af-store-remix.vercel.app/',
+    github: "https://github.com/BrianC9/af-store-remix",
+    preview: "https://af-store-remix.vercel.app/",
   },
   technologies: [
     {
-      id: 'remix',
-      name: 'React JS',
+      id: "remix",
+      name: "React JS",
     },
 
     {
-      id: 'tailwind',
-      name: 'Tailwind CSS',
+      id: "tailwind",
+      name: "Tailwind CSS",
     },
     {
-      id: 'nextjs',
-      name: 'Next',
+      id: "nextjs",
+      name: "Next",
     },
-    { id: 'java', name: 'Java' },
+    { id: "java", name: "Java" },
   ],
   images: {
-    cover: 'https://picsum.photos/1280/720',
+    cover: "https://picsum.photos/1280/720",
     collection: [
       {
-        src: 'https://picsum.photos/1280/720',
+        src: "https://picsum.photos/1280/720",
         alt: {
-          en: 'Homepage from AF Store',
-          es: 'Página de incio de AF Store',
+          en: "Homepage from AF Store",
+          es: "Página de incio de AF Store",
         },
       },
       {
-        src: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+        src: "https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80",
         alt: {
-          en: 'Homepage from AF Store',
-          es: 'Página de incio de AF Store',
+          en: "Homepage from AF Store",
+          es: "Página de incio de AF Store",
         },
       },
       {
-        src: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+        src: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
         alt: {
-          en: 'Homepage from AF Store',
-          es: 'Página de incio de AF Store',
+          en: "Homepage from AF Store",
+          es: "Página de incio de AF Store",
         },
       },
     ],
@@ -78,80 +79,80 @@ function Project() {
   const { title, longDescription, technologies, images, links } = buscado;
   return (
     <MainLayout page={title}>
-      <main className='px-5 sm:px-10 md:px-15 lg:px-20 xl:px-30 max-w-[85ch] mx-auto'>
+      <main className="px-5 sm:px-10 md:px-15 lg:px-20 xl:px-30 max-w-[85ch] mx-auto">
         <section
-          id='heading'
-          className='grid gap-4 grid-cols-1 place-content-center place-items-center w-2/4 mx-auto'
+          id="heading"
+          className="grid gap-4 grid-cols-1 place-content-center place-items-center w-2/4 mx-auto"
         >
-          <h1 className='text-2xl md:text-4xl uppercase text-center'>
+          <h1 className="text-2xl md:text-4xl uppercase text-center">
             {title}
           </h1>
           <Image
-            src={'/assets/images/undraw-project.svg'}
-            alt='A person launching a rocket'
+            src={"/assets/images/undraw-project.svg"}
+            alt="A person launching a rocket"
             width={400}
             height={400}
-            className=' w-full md:w-2/4'
+            className=" w-full md:w-2/4"
             priority={true}
-            blurDataURL={'/assets/images/undraw-project.svg'}
-            placeholder='blur'
+            blurDataURL={"/assets/images/undraw-project.svg"}
+            placeholder="blur"
           />
         </section>
-        <section className='grid gap-5 my-6 max-w-[85ch] mx-auto text-justify'>
-          <h2 className='text-2xl  '>
-            Project <span className='text-secondary'>details</span>
+        <section className="grid gap-5 my-6 max-w-[85ch] mx-auto text-justify">
+          <h2 className="text-2xl  ">
+            Project <span className="text-secondary">details</span>
           </h2>
-          <h3 className='text-xl'>Description</h3>
+          <h3 className="text-xl">Description</h3>
           <p>{longDescription.en}</p>
 
-          <h3 className='text-xl'>Tech stack</h3>
-          <div className='my-3 flex gap-4 '>
+          <h3 className="text-xl">Tech stack</h3>
+          <div className="my-3 flex gap-4 ">
             {technologies.map((tech) => (
               <div
                 key={tech.id}
-                className='flex flex-col items-center content-center justify-center'
+                className="flex flex-col items-center content-center justify-center"
               >
                 <span>{iconResolver(tech.id, 35, tech.name)}</span>
-                <p className='font-semibold'>{tech.name}</p>
+                <p className="font-semibold">{tech.name}</p>
               </div>
             ))}
           </div>
         </section>
-        <section className='my-6 max-w-[85ch] mx-auto'>
-          <div className=' gap-6 flex flex-col sm:flex-row'>
+        <section className="my-6 max-w-[85ch] mx-auto">
+          <div className=" gap-6 flex flex-col sm:flex-row">
             <a
               href={links.preview}
-              target='_blank'
-              rel='noreferrer'
-              className='flex items-center gap-2'
+              target="_blank"
+              rel="noreferrer"
+              className="flex items-center gap-2"
             >
-              <h2 className='text-2xl '>
-                View <span className='text-secondary'>project</span>
+              <h2 className="text-2xl ">
+                View <span className="text-secondary">project</span>
               </h2>
-              <div className='block'>
-                {iconResolver('redirect', 22, 'View project')}
+              <div className="block">
+                {iconResolver("redirect", 22, "View project")}
               </div>
             </a>
             <a
               href={links.github}
-              target='_blank'
-              rel='noreferrer'
-              className='flex items-center gap-2'
+              target="_blank"
+              rel="noreferrer"
+              className="flex items-center gap-2"
             >
-              <h2 className='text-2xl inline-block '>
-                Review <span className='text-secondary'>code</span>
+              <h2 className="text-2xl inline-block ">
+                Review <span className="text-secondary">code</span>
               </h2>
-              {iconResolver('github', 22, 'Review code')}
+              {iconResolver("github", 22, "Review code")}
             </a>
           </div>
 
           <Slider slides={images.collection} />
         </section>
-        <section className='py-10'>
-          <Link href={'/projects'}>
-            <h3 className=' '>
-              <span className=''>Go back </span>
-              <span className='hover:underline underline-offset-4 text-secondary transition-all duration-500 ease-in-out'>
+        <section className="py-10">
+          <Link href={"/projects"}>
+            <h3 className=" ">
+              <span className="">Go back </span>
+              <span className="hover:underline underline-offset-4 text-secondary transition-all duration-500 ease-in-out">
                 to projects
               </span>
             </h3>
