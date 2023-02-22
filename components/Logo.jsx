0e0c731e053c0953f682e4caba070b0b0234ff6c
Bryan Cusme <bryan.cusme@devoteam.com>
2023-02-22 09:31:30 +0100

    add: color scheme

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -3,9 +3,11 @@ function Logo() {
   return (
     <Link href={"/"}>
       <div className="text-4xl md:text-5xl ">
-        <span className="text-primary font-bold ">{`<`}</span>
-        <span className="text-secondary font-bold">BC</span>
-        <span className="text-primary font-bold">{`>`}</span>
+        <span className="text-primary font-extrabold ">{`<`}</span>
+        <span className="text-secondary font-bold tracking-tighter mx-[0.15rem] ">
+          BC
+        </span>
+        <span className="text-primary font-extrabold ">{`>`}</span>
       </div>
     </Link>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#141D2C',
-        secondary: '#4792FC',
-        neutral: '#E3E3F1',
+        primary: "#37476c",
+        secondary: "#0EA5E9",
+        neutral: "#ffffff",
+        light: "#e6ecf3",
+        mid: "#CBD5E1",
+        darkest: "#0F172A",
       },
       fontFamily: {
-        sans: ['var(--font-rubik)', 'sans-serif'],
+        sans: ["var(--font-rubik)", "sans-serif"],
       },
       keyframes: {
         wave: {
-          '0%': { transform: 'rotate(0.0deg)' },
-          '10%': { transform: 'rotate(14deg)' },
-          '20%': { transform: 'rotate(-8deg)' },
-          '30%': { transform: 'rotate(14deg)' },
-          '40%': { transform: 'rotate(-4deg)' },
-          '50%': { transform: 'rotate(10.0deg)' },
-          '60%': { transform: 'rotate(0.0deg)' },
-          '100%': { transform: 'rotate(0.0deg)' },
+          "0%": { transform: "rotate(0.0deg)" },
+          "10%": { transform: "rotate(14deg)" },
+          "20%": { transform: "rotate(-8deg)" },
+          "30%": { transform: "rotate(14deg)" },
+          "40%": { transform: "rotate(-4deg)" },
+          "50%": { transform: "rotate(10.0deg)" },
+          "60%": { transform: "rotate(0.0deg)" },
+          "100%": { transform: "rotate(0.0deg)" },
         },
       },
       animation: {
-        'waving-hand': 'wave 3s linear infinite',
+        "waving-hand": "wave 3s linear infinite",
       },
     },
   },
