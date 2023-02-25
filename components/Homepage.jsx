0189d5b527cb070472e29f62bf03a747fe3b0106
Bryan Cusme <bryan.cusme@devoteam.com>
2023-02-25 22:05:37 +0100

    fix: homepage layout

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 import SocialIcons from "./SocialIcons";
 function Homepage() {
   return (
-    <main className="max-w-[85ch] mx-auto flex-col md:flex md:flex-row items-center  justify-center gap-14  px-5 sm:px-10 md:px-15 lg:px-20 xl:px-30 md:grid-cols-2 ">
+    <main className="max-w-[85ch] mx-auto flex flex-col md:flex md:flex-row items-center  justify-center gap-14  px-5 sm:px-10 md:px-15 lg:px-20 xl:px-30 md:grid-cols-2 ">
       <Image
         src={"/assets/images/foto-perfil.jpg"}
         alt="Myself portrait"
         width={400}
         height={400}
-        className="rounded-full"
+        className="rounded-full w-[95%] sm:w-[80%] md:w-full"
         priority={true}
         blurDataURL={"/assets/images/foto-perfil.jpg"}
         placeholder="blur"
       />
       <section className="flex flex-col items-center  gap-2 md:text-left md:items-start  ">
-        <div className="self-start  sm:pl-6 md:pl-0 text-3xl md:text-4xl  ">
+        <div className="  sm:pl-6 md:pl-0 text-3xl md:text-4xl  ">
           <h1 className="typewritter-animation">
             Hi I`m <span className="text-secondary">Bryan</span>
           </h1>
